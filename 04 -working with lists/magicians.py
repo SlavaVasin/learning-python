07bd050e42d76bbc83bc09958084e4ab09e47237
Vasin Viacheslav <in-lite@list.ru>
2022-11-14 14:48:58 +0500

    working with lists

---
+++ 04 -working with lists/magicians.py
@@ -0,0 +1,12 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+    
+print()
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ', that was a trick')
+    print("I can't wait to see younext trick, " + magician.title() + 
+          '\n')
+print("Thank you, everyone. That was a great magic show!")
+
