b525066ab724737ee1bdd94c7fa1c7e2c40193d3
Vasin Viacheslav <in-lite@list.ru>
2022-12-01 13:55:39 +0500

    fun

---
+++ 09 - function/making_pizza.py
@@ -0,0 +1,7 @@
+# import pizza
+from pizza import make_pizza as mp
+
+
+
+mp(16, 'pepperoni')
+mp(12, 'extra cheese', 'moshrooms', 'green peppers')
--- 09 - function/pizza.py
+++ 09 - function/pizza.py
@@ -3,5 +3,3 @@ def make_pizza(size, *toppings):
     for i in toppings:
         print("- " + i)
     
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'extra cheese', 'moshrooms', 'green peppers')
